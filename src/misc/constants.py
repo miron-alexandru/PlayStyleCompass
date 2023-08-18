f0f30b29b7131263d9c412ca6aa1f7b456007cd9
Ake <khadaake@gmail.com>
2023-08-18 19:16:11 +0300

    modified constants.

--- src/misc/constants.py
+++ src/misc/constants.py
@@ -13,7 +13,8 @@ Wii (36)
 
 platform_ids = ['94', '17', '123', '96', '21', '52', '32', '22', '152', '140', '36' ]
 
-API_KEY = '8350ec49b114e2c880d043c324ed6b851706d459'
+# Modify API Key
+API_KEY = ''
 
 # URL
 BASE_URL = 'https://www.giantbomb.com/api/'
