1eaf58e0ea47f77f390b60e347dd788bedb15a5e
Ake <khadaake@gmail.com>
2023-08-31 17:30:42 +0300

    update

--- src/playstyle_manager/wsgi.py
+++ src/playstyle_manager/wsgi.py
@@ -2,7 +2,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 # Set the environment variable to indicate production mode
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'playstyle_managersd.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'playstyle_compass.settings')
 
 # Get the Django WSGI application
 application = get_wsgi_application()
