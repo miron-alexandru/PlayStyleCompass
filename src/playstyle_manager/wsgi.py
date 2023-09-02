4221f697d18c81e5d170f9a0b3bc5a7ae264e70b
Ake <khadaake@gmail.com>
2023-09-02 18:27:35 +0300

    test heroku

--- src/playstyle_manager/wsgi.py
+++ src/playstyle_manager/wsgi.py
@@ -3,7 +3,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 # Set the environment variable to indicate production mode
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'playstyle_manager.settings')
 
 # Get the Django WSGI application
 application = get_wsgi_application()
