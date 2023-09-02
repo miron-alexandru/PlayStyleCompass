cc6368df7b4f37f11cf84ce97f9a15eaaa5c4830
Ake <khadaake@gmail.com>
2023-09-02 18:53:16 +0300

    test heroku

--- src/playstyle_manager/settings.py
+++ src/playstyle_manager/settings.py
@@ -7,8 +7,9 @@ from dotenv import load_dotenv
 load_dotenv()
 # Base Dir
 BASE_DIR = os.path.dirname(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.insert(0, os.path.join(BASE_DIR, 'playstyle_compass'))
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
-sys.path.append(os.path.abspath(BASE_DIR + 'playstyle_compass'))
+
 
 DEBUG = True
 
