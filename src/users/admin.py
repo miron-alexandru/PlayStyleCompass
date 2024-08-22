e82b2c314d4f13e378769f9f7217215465e0ef41
Ake <khadaake@gmail.com>
2024-08-22 15:22:57 +0300

    Updated instructions on how to run the project locally.

--- src/users/admin.py
+++ src/users/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-#from modeltranslation.admin import TranslationAdmin
 
 from .models import (
     UserProfile,
@@ -12,10 +11,6 @@ from .models import (
     QuizUserResponse,
     ChatMessage,
 )
-
-
-#class QuizAdmin(TranslationAdmin):
- #   pass
 
 
 admin.site.register(QuizQuestion)
