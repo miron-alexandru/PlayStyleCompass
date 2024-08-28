c648fd2bd55d495807fd77f469f49c20e9929b31
Ake <khadaake@gmail.com>
2024-08-28 13:47:09 +0300

    testing google cloud storage.

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 
-export DJANGO_SUPERUSER_USERNAME=admin
-export DJANGO_SUPERUSER_EMAIL=admin@example.com
-export DJANGO_SUPERUSER_PASSWORD=adminpassword
-python manage.py createsuperuser --noinput
+#export DJANGO_SUPERUSER_USERNAME=admin
+#export DJANGO_SUPERUSER_EMAIL=admin@example.com
+#export DJANGO_SUPERUSER_PASSWORD=adminpassword
+#python manage.py createsuperuser --noinput
