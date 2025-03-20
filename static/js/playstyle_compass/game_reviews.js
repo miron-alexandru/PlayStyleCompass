015af9f8968abdddb9563c03861f2716dca74145
Ake <khadaake@gmail.com>
2025-03-20 11:04:01 +0200

    Users are able to like or dislike reviews on the Game Reviews page.

--- static/js/playstyle_compass/game_reviews.js
+++ static/js/playstyle_compass/game_reviews.js
@@ -14,3 +14,54 @@ document.addEventListener("DOMContentLoaded", function () {
     element.innerHTML = getStarRating(score);
   });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const showMessage = (container, message) => {
+    let customMessage = $(`<div class="success-message">${message}</div>`);
+    container.append(customMessage);
+
+    setTimeout(() => {
+      customMessage.fadeOut("slow", function () {
+        customMessage.remove();
+      });
+    }, 3000);
+  };
+
+  const sendRatingAction = (actionType, event) => {
+    event.preventDefault();
+
+    let thisContainer = $(event.target).closest(".like-dislike");
+    let reviewId = thisContainer.closest(".review").data("review-id");
+    let ratingUrl = thisContainer.data(`${actionType}-url`);
+
+    $.ajax({
+      type: "POST",
+      url: ratingUrl,
+      headers: {
+        "X-CSRFToken": csrfToken,
+      },
+      data: {
+        review_id: reviewId,
+      },
+      success: function (data) {
+        showMessage(thisContainer, data["message"]);
+
+        // Update like and dislike counts
+        thisContainer.find(".like-count").text(data.likes);
+        thisContainer.find(".dislike-count").text(data.dislikes);
+      },
+      error: function (error) {
+        console.error(`Error incrementing ${actionType}:`, error);
+      },
+    });
+  };
+
+  // Attach event listeners
+  $(document).on("click", ".thumbs-up", function (event) {
+    sendRatingAction("like", event);
+  });
+
+  $(document).on("click", ".thumbs-down", function (event) {
+    sendRatingAction("dislike", event);
+  });
+});
