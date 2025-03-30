abcc536bb3c90d9f82b60900dc55534976de6fdb
Ake <khadaake@gmail.com>
2025-03-30 18:01:16 +0300

    Added read more and read less functionality for game reviews description / In game reviews: An error message appears when users try to view the profile of a non-existent user.

--- static/js/playstyle_compass/game_reviews.js
+++ static/js/playstyle_compass/game_reviews.js
@@ -8,10 +8,81 @@ const getStarRating = (score) => {
   return starsHtml;
 };
 
+
+function decodeHTMLEntities(text) {
+  let textArea = document.createElement("textarea");
+  textArea.innerHTML = text;
+  return textArea.value;
+}
+
+document.querySelectorAll(".review-description-container").forEach(function (container) {
+  let truncatedText = container.querySelector(".review-description");
+  let fullText = container.querySelector(".review-description-full");
+  let readButton = container.querySelector(".read-button-review");
+
+  truncatedText.innerHTML = decodeHTMLEntities(truncatedText.innerHTML);
+  fullText.innerHTML = decodeHTMLEntities(fullText.innerHTML);
+
+  if (truncatedText.textContent.trim() === fullText.textContent.trim()) {
+    readButton.style.display = "none";
+  } else {
+    readButton.style.display = "inline";
+  }
+
+  readButton.addEventListener("click", function () {
+    if (truncatedText.style.display === "none") {
+      truncatedText.style.display = "inline";
+      fullText.style.display = "none";
+      readButton.textContent = translate("[Read more...]");
+    } else {
+      truncatedText.style.display = "none";
+      fullText.style.display = "inline";
+      readButton.textContent = translate("[Read less...]");
+    }
+  });
+});
+
+
 document.addEventListener("DOMContentLoaded", function () {
   document.querySelectorAll(".star-rating").forEach((element) => {
     const score = parseInt(element.getAttribute("data-score"), 10);
     element.innerHTML = getStarRating(score);
+  });
+});
+
+
+const showUserMessage = (container, message) => {
+  let customMessage = document.createElement("div");
+  customMessage.className = "error-message";
+  customMessage.textContent = message;
+  container.appendChild(customMessage);
+
+  setTimeout(() => {
+    customMessage.classList.add("fade-out");
+    setTimeout(() => {
+      customMessage.remove();
+    }, 1000);
+  }, 3000);
+};
+
+document.querySelectorAll(".author-name").forEach((link) => {
+  link.addEventListener("click", function (event) {
+    event.preventDefault();
+    let profileUrl = this.href;
+    let container = this.closest(".author-container");
+
+    fetch(profileUrl, { headers: { "X-Requested-With": "XMLHttpRequest" } })
+      .then((response) => response.json())
+      .then((data) => {
+        if (!data.exists) {
+          showUserMessage(container, "User does not exist or has deleted their account!");
+        } else {
+          window.open(profileUrl, "_blank");
+        }
+      })
+      .catch(() => {
+        window.open(profileUrl, "_blank");
+      });
   });
 });
 
