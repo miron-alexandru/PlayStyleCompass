46e6546986f2d360698013da0d79916813c21041
Ake <khadaake@gmail.com>
2025-10-27 15:54:33 +0200

    Solved a BUG which caused the game rating stars to never appear filled.

--- static/js/playstyle_compass/star_rating.js
+++ static/js/playstyle_compass/star_rating.js
@@ -2,10 +2,9 @@ const starWidth = 20;
 
 $.fn.stars = function () {
   return $(this).each(function () {
+    const rating = Math.max(0, Math.min(5, parseFloat($(this).html())));
     $(this).html(
-      $("<span />").width(
-        Math.max(0, Math.min(5, parseFloat($(this).html()))) * starWidth
-      )
+      $('<span class="filled" />').width(rating * starWidth)
     );
   });
 };
